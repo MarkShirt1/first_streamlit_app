cd3caf76358d8644e72e7221cbe4b7b2062e5b53
MarkShirt1 <136693041+MarkShirt1@users.noreply.github.com>
2023-09-21 15:04:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Bacon Buttie')
-streamlit.txt('Sausage and Egg Sandwich')
+streamlit.text('Sausage and Egg Sandwich')
